554dbe58376de6ba1e585a1cff320fd425a3dc57
wondavibes <amosayemoro2001@gmail.com>
2025-11-16 23:21:57 +0100

    serializers

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import User, Message, Conversation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "user_id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "role",
+            "created_at",
+        ]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = [
+            "message_id",
+            "sender",
+            "conversation",
+            "message_body",
+            "sent_at",
+        ]
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = [
+            "conversation_id",
+            "participants",
+            "created_at",
+            "messages",
+        ]
