efbc5098f833e2e0c0805dc34d1335a5320cfef2
wondavibes <amosayemoro2001@gmail.com>
2025-11-16 17:06:45 +0100

    decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,14 @@ class TestAccessNestedMap(unittest.TestCase):
     )
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), "'a'"),
+            ({"a": 1}, ("a", "b"), "'b'"),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, expected_message):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), expected_message)
