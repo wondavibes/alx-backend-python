fe816e1e0f509708519ea0a06d6fbfd2c31a690e
wondavibes <amosayemoro2001@gmail.com>
2025-11-23 13:20:08 +0100

    tried path rename

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter  # type: ignore
 
 from .views import ConversationViewSet, MessageViewSet
 
@@ -8,8 +8,12 @@ router = DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 
 # nested router to expose messages under a conversation
-conversations_router = NestedDefaultRouter(router, r"conversations", lookup="conversation")
-conversations_router.register(r"messages", MessageViewSet, basename="conversation-messages")
+conversations_router = NestedDefaultRouter(
+    router, r"conversations", lookup="conversation"
+)
+conversations_router.register(
+    r"messages", MessageViewSet, basename="conversation-messages"
+)
 
 # expose both top-level and nested routes
 urlpatterns = router.urls + conversations_router.urls
